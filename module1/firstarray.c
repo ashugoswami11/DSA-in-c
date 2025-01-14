f3cf7d6c63c3f9dd6a527476840b3399d6d2b9c7
Ashutosh Goswami <ashugoswami6737@gmail.com>
2025-01-14 21:03:51 +0530

    commit 1

---
+++ module1/firstarray.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//initializing a stucture to hold arrays
+struct myarray{
+    int total_size;
+    int used_size;
+    int *ptr;
+};
+
+void createarray(struct myarray* a, int tsize, int usize){
+    a->total_size = tsize;
+    a->used_size = usize;
+    a->ptr = (int*)malloc(tsize*sizeof(int));
+}
+
+int main(){
+    struct myarray marks;
+    createarray(&marks, 100, 20);
+
+return 0;
+}
---
+++ module1/readme.txt
@@ -0,0 +1,4 @@
+this module is related to ADT(abstract data type) Arrays.
+coded in c language.
+
+with the help of code with harry dsa course vid# 8
