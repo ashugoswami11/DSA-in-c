4fc5b1d8e5bf509b67f4341fbc683a11c7a8a41f
Ashutosh Goswami <ashugoswami6737@gmail.com>
2025-03-01 21:16:13 +0530

    commit 33

--- module 6(sorting_algorithms)/mergesort.c
+++ module 6(sorting_algorithms)/mergesort.c
@@ -8,53 +8,7 @@ void printArray(int* A, int n){
     printf("\n");
 }
 
-void merge(int * A, int mid, int low, int high){
-    int i,j,k, B[100];
-    i = low;
-    j = mid+1;
-    k = low;
 
-    while (i<=mid && j <= high)
-    {
-        if(A[i]<A[j]){
-            B[k] = A[i];
-            k++;
-            i++;
-        }
-        else{
-            B[k] = A[j];
-            j++;
-            k++;
-        }
-    }
-
-    while(i<=mid){
-        B[k] = A[i];
-        k++;
-        i++;
-    }
-
-    while(j<=high){
-        B[k] = A[j];
-        k++;
-        j++;
-    }
-
-    for(int i = low; i<=high; i++){
-        A[i] = B[i];
-    }
-    
-}
-
-void mergeSort(int * A, int low , int high){
-    int mid;
-    if(low<high){
-        mid = (low+high)/2;
-        mergeSort(A,low,mid);
-        mergeSort(A,mid+1,high);
-        merge(A,mid,low,high);
-    }
-}
 
 int main(){
 
