49a64e62104321f15259777502bd8b455f41fda4
Ashutosh Goswami <ashugoswami6737@gmail.com>
2025-03-02 18:47:52 +0530

    commit 34

--- module 6(sorting_algorithms)/mergesort.c
+++ module 6(sorting_algorithms)/mergesort.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-void printArray(int* A, int n){
+void printArray(int *A, int n)
+{
     for (int i = 0; i < n; i++)
     {
         printf("%d ", A[i]);
@@ -8,14 +9,68 @@ void printArray(int* A, int n){
     printf("\n");
 }
 
+void merge(int *A, int mid, int low, int high)
+{
+    int i, j, k, B[100];
+    i = low;
+    j = mid + 1;
+    k = low;
 
+    while (i <= mid && j <= high)
+    {
+        if (A[i] < A[j])
+        {
+            B[k] = A[i];
+            i++;
+            k++;
+        }
+        else
+        {
+            B[k] = A[j];
+            k++;
+            j++;
+        }
+    }
 
-int main(){
+    while(i <= mid){
+        B[k] = A[i] ;
+        k++;
+        i++;
+    }
+    while (j<=high)
+    {
+        B[k] = A[j] ;
+        k++;
+        j++;
+    }
 
+    for(int i = low; i<=high; i++){
+        A[i]= B[i];
+    }
+    
+}
+
+void mergesort(int *A, int low, int high)
+{
+    int mid;
+
+    if (low < high)
+    {
+        mid = (low + high) / 2;
+        mergesort(A, low, mid);
+        mergesort(A, mid + 1, high);
+        merge(A, mid, low, high);
+    }
+}
+
+int main()
+{
+    // int A[] = {9, 14, 4, 8, 7, 5, 6};
     int A[] = {9, 1, 4, 14, 4, 15, 6};
     int n = 7;
     printArray(A, n);
-    mergeSort(A, 0, 6);
+    mergesort(A, 0, n-1);
+    // quicksort(A, 0, n - 1);
     printArray(A, n);
-
+    return 0;
 }
