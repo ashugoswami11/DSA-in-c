0946bcb93b7af6953c676f5766078a5774663255
Ashutosh Goswami <ashugoswami6737@gmail.com>
2025-03-03 17:44:40 +0530

    commit 35

--- module 6(sorting_algorithms)/merger.c
+++ module 6(sorting_algorithms)/merger.c
@@ -9,57 +9,56 @@ void printArray(int *A, int n)
     printf("\n");
 }
 
-void merge(int *A, int mid, int low, int high)
-{
-    int i, j, k, B[100];
+void merge(int* A, int low, int mid, int high){
+    int i ,j , k, B[100];
     i = low;
-    j = mid + 1;
+    j = mid+1;
     k = low;
 
-    while (i <= mid && j <= high)
+    while (i<=mid && j<=high)
     {
-        if (A[i] < A[j])
+        if (A[i]<A[j])
         {
             B[k] = A[i];
             i++;
             k++;
         }
+
         else
         {
             B[k] = A[j];
-            k++;
             j++;
+            k++;
         }
+        
     }
 
-    while(i <= mid){
-        B[k] = A[i] ;
-        k++;
+    while(i<=mid){
+        B[k]=A[i];
         i++;
-    }
-    while (j<=high)
-    {
-        B[k] = A[j] ;
         k++;
-        j++;
     }
 
-    for(int i = low; i<=high; i++){
-        A[i]= B[i];
+    while(j<=high){
+        B[k]=A[j];
+        j++;
+        k++;
+    }
+    
+    for (int i = low; i <= high; i++)
+    {
+        A[i] = B[i];
     }
     
 }
 
-void mergesort(int *A, int low, int high)
-{
+void mergesort(int *A, int low , int high){
     int mid;
-
-    if (low < high)
-    {
-        mid = (low + high) / 2;
-        mergesort(A, low, mid);
-        mergesort(A, mid + 1, high);
-        merge(A, mid, low, high);
+    if(low<high){
+        mid = (low+high)/2;
+        mergesort(A, low , mid);
+        mergesort(A, mid+1 , high);
+        merge(A,low,mid,high);
     }
 }
 
